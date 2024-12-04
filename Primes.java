9e341337dd8a8479d6fe080d46ddcd4cb2bc9f68
Noam Bavli <noambav@gmail.com>
2024-12-04 18:43:06 +0200

    added initial print

--- Primes.java
+++ Primes.java
@@ -2,6 +2,7 @@ public class Primes {
     public static void main(String[] args) {
         // Replace this statement with your code
         final int N = Integer.parseInt(args[0]);
+        System.out.println("Prime numbers up to " + N + ":");
         boolean[] isPrime = new boolean[N];
         int currentPrime = 2;
         for (int i = 0; i < isPrime.length; i++) {
@@ -28,6 +29,7 @@ public class Primes {
                 counter++;
             }
         }
+
         System.out.println(
                 "There are " + counter + " primes between 2 and " + N + " (" + (int) (100.0 * counter / N)
                         + "% are prime)");
