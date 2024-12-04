9f7ef439c648fd35fbf6535ec79e292a731b0997
Noam Bavli <noambav@gmail.com>
2024-12-04 18:55:03 +0200

    added s for plural in prime last string ........................

--- Primes.java
+++ Primes.java
@@ -32,6 +32,6 @@ public class Primes {
 
         System.out.println(
                 "There are " + counter + " primes between 2 and " + N + " (" + (int) (100.0 * counter / N)
-                        + "% are prime)");
+                        + "% are primes)");
     }
 }
